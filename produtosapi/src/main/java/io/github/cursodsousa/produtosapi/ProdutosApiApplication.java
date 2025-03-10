92130fa009409e0d710c32d30b351599b5d5981b
mfernandasdias <91575360+mfernandadias@users.noreply.github.com>
2025-03-09 22:50:53 -0300

    avança 06

---
+++ produtosapi/src/main/java/io/github/cursodsousa/produtosapi/ProdutosApiApplication.java
@@ -0,0 +1,23 @@
+package io.github.cursodsousa.produtosapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@SpringBootApplication
+@RestController
+public class ProdutosApiApplication {
+
+	@GetMapping("/hello-world")
+	public String helloWord(){
+		return "Hello World";
+	}
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(ProdutosApiApplication.class, args);
+	}
+
+}
