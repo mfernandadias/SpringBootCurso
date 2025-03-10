68a04174cc5386dba47be4d525bfae8876510bfa
mfernandasdias <91575360+mfernandadias@users.noreply.github.com>
2025-03-09 22:49:30 -0300

    teste 01

---
+++ produtosapi/src/main/java/io/github/cursodsousa/produtosapi/controller/ProdutoController.java
@@ -0,0 +1,29 @@
+package io.github.cursodsousa.produtosapi.controller;
+
+
+import io.github.cursodsousa.produtosapi.model.Produto;
+import io.github.cursodsousa.produtosapi.repository.ProdutoRepository;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("produtos")
+public class ProdutoController {
+    private ProdutoRepository produtoRepository;
+
+    public ProdutoController(ProdutoRepository produtoRepository) {
+        this.produtoRepository = produtoRepository;
+    }
+
+    @PostMapping
+    public void salvar(@RequestBody Produto produto){
+        System.out.println("Produto recebido: " + produto);
+
+        //return produto;
+    }
+
+
+}
