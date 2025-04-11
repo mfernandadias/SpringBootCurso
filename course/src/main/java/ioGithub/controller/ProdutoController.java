bd349cd880a3141bf38ec9e2994be757a5b88d32
Fernanda Dias <mariadosantodias@gmail.com>
2025-04-10 21:49:52 -0300

    produto controller

---
+++ course/src/main/java/ioGithub/controller/ProdutoController.java
@@ -0,0 +1,32 @@
+package ioGithub.controller;
+
+import ioGithub.model.Produto;
+import ioGithub.repository.ProdutoRepository;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("produtos")
+public class ProdutoController {
+
+    private ProdutoRepository produtoRepository;
+
+    public ProdutoController(ProdutoRepository produtoRepository){
+        this.produtoRepository = produtoRepository;
+    }
+
+    @PostMapping
+    public Produto salvar(@RequestBody Produto produto){
+        System.out.println("PRoduto recebido: " + produto);
+
+        var id = UUID.randomUUID().toString();
+        produto.setId(id);
+
+        produtoRepository.save(produto);
+        return produto;
+    }
+}
