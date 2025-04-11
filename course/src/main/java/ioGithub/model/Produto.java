7850099ae8119b0facfb475746f42e2121021c39
Fernanda Dias <mariadosantodias@gmail.com>
2025-04-10 21:49:59 -0300

    produto

---
+++ course/src/main/java/ioGithub/model/Produto.java
@@ -0,0 +1,59 @@
+package ioGithub.model;
+
+//POJO -> Plain Old Java Object
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+
+@Entity
+@Table(name = "produto")
+public class Produto {
+
+    @Id
+    @Column(name = "id")
+    private String id;
+
+    @Column(name = "nome")
+    private String nome;
+
+    @Column(name = "descricao")
+    private String descricao;
+
+    @Column(name = "preco")
+    private Double preco;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
