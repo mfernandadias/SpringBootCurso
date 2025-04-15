1a01ca48b5b1c0b106dceedd264c1082eafa0ae7
Fernanda Dias <mariadosantodias@gmail.com>
2025-04-14 23:10:03 -0300

    delete produto repository

--- course/src/main/java/ioGithub/repository/ProdutoRepository.java
+++
@@ -1,7 +0,0 @@
-package ioGithub.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import  ioGithub.model.Produto;
-
-public interface ProdutoRepository extends JpaRepository<Produto, String> {
-}
