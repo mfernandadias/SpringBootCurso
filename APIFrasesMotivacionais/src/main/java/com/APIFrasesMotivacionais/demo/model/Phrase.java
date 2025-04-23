027ea89250b206fe2dc4f6865ab710d9a5bdb400
Fernanda Dias <mariadosantodias@gmail.com>
2025-04-22 23:44:13 -0300

    application test

---
+++ APIFrasesMotivacionais/src/main/java/com/APIFrasesMotivacionais/demo/model/Phrase.java
@@ -0,0 +1,22 @@
+package com.APIFrasesMotivacionais.demo.model;
+
+public class Phrase {
+    private Long id;
+    private String content;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
