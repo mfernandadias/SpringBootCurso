43ef597cd03e59966b8aeb82200b54db2a8eee6a
Fernanda Dias <mariadosantodias@gmail.com>
2025-04-22 23:45:29 -0300

    produto repository

--- produtosapi/src/main/java/io/github/cursodsousa/produtosapi/repository/ProdutoRepository.java
+++ produtosapi/src/main/java/io/github/cursodsousa/produtosapi/repository/ProdutoRepository.java
@@ -1,5 +1,11 @@
 package io.github.cursodsousa.produtosapi.repository;
 
-public interface ProdutoRepository extends JpsReposity{
+import io.github.cursodsousa.produtosapi.model.Produto;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface  ProdutoRepository extends JpaRepository<Produto, String> {
+    List<Produto> findByNome(String nome);
 
 }
