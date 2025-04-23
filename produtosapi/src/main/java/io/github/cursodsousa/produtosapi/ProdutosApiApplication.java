c62bb040f09e7d18ac949e697e3dba9728069581
Fernanda Dias <mariadosantodias@gmail.com>
2025-04-22 23:45:10 -0300

    api aplication java

--- produtosapi/src/main/java/io/github/cursodsousa/produtosapi/ProdutosApiApplication.java
+++ produtosapi/src/main/java/io/github/cursodsousa/produtosapi/ProdutosApiApplication.java
@@ -2,27 +2,13 @@ package io.github.cursodsousa.produtosapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 
 @SpringBootApplication
-@RestController
+
 public class ProdutosApiApplication {
 
-	@GetMapping("/hello-world")
-	public String helloWord(){
-		return "Hello World";
-	}
-
 	public static void main(String[] args) {
-
 		SpringApplication.run(ProdutosApiApplication.class, args);
 	}
 
 }
-
-/*
-Cadastro de produto
-Classe
- */
