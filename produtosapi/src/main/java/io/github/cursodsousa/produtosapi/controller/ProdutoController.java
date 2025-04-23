9b4a5f0d6d6eedd53385e77d4240d5dcc847b89f
Fernanda Dias <mariadosantodias@gmail.com>
2025-04-22 23:44:23 -0300

    produto controller

--- produtosapi/src/main/java/io/github/cursodsousa/produtosapi/controller/ProdutoController.java
+++ produtosapi/src/main/java/io/github/cursodsousa/produtosapi/controller/ProdutoController.java
@@ -2,28 +2,17 @@ package io.github.cursodsousa.produtosapi.controller;
 
 
 import io.github.cursodsousa.produtosapi.model.Produto;
-import io.github.cursodsousa.produtosapi.repository.ProdutoRepository;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
-@RestController
-@RequestMapping("produtos")
+@RestController //controlar rest
+@RequestMapping("produtos") //qual a sua utilidade de requestMapping ?
 public class ProdutoController {
-    private ProdutoRepository produtoRepository;
 
-    public ProdutoController(ProdutoRepository produtoRepository) {
-        this.produtoRepository = produtoRepository;
-    }
-
-    @PostMapping("/")
-    public void salvar(@RequestBody Produto produto){
+    @PostMapping
+    public void salvar(@RequestBody Produto produto){ //qual a utilidade da "RequestBody
         System.out.println("Produto recebido: " + produto);
-
-        //return produto;
     }
-
-
 }
