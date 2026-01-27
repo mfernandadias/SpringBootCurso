32475e38d0b73c33f5f307a318f557ee87870f9b
Fernanda Dias <mariadosantodias@gmail.com>
2026-01-27 20:30:57 -0300

    gestao application tests

---
+++ gestao-fest/src/test/java/com/algaworks/gestao_fest/GestaoFestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.algaworks.gestao_fest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoFestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
