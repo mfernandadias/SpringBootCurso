6d53ba1df1857198bf5ade3ceffeba8bd90bc131
Fernanda Dias <mariadosantodias@gmail.com>
2026-01-29 10:40:42 -0300

    ninja model

---
+++ rest-with-spring-boot-and-java-produtoTest/src/main/java/br/com/erudio/Ninjas/NinjaModel.java
@@ -0,0 +1,53 @@
+package br.com.erudio.Ninjas;
+
+
+import br.com.erudio.Missoes.Missoes;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_cadastro")
+public class NinjaModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String email;
+    private int idade;
+    private List<Missoes> missoes;
+
+    public NinjaModel() {
+    }
+
+    public NinjaModel(String nome, int idade, String email) {
+        this.nome = nome;
+        this.idade = idade;
+        this.email = email;
+    }
+
+    //ideal colocar id
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
