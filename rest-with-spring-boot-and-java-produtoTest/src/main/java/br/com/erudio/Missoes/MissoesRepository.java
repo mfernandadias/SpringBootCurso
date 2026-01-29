ce385de396393d40aca1fae2ac07639a43bef04c
Fernanda Dias <mariadosantodias@gmail.com>
2026-01-29 10:40:05 -0300

    missoes repository

---
+++ rest-with-spring-boot-and-java-produtoTest/src/main/java/br/com/erudio/Missoes/MissoesRepository.java
@@ -0,0 +1,7 @@
+package br.com.erudio.Missoes;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MissoesRepository extends JpaRepository<MissoesModel, Long> {
+    
+}
