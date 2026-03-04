dcdefb594e18e1e64f9372acc59c520e013c8f82
Fernanda Dias <mariadosantodias@gmail.com>
2026-03-04 16:39:30 -0300

    Update MissoesRepository.java

--- rest-with-spring-boot-and-java-produtoTest/src/main/java/br/com/erudio/Missoes/MissoesRepository.java
+++ rest-with-spring-boot-and-java-produtoTest/src/main/java/br/com/erudio/Missoes/MissoesRepository.java
@@ -3,5 +3,9 @@ package br.com.erudio.Missoes;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MissoesRepository extends JpaRepository<MissoesModel, Long> {
-    
+
+
+
+
+
 }
