ad1a87aee5f8608c5667588721ad337ad3e97973
Fernanda Dias <mariadosantodias@gmail.com>
2026-03-04 16:39:34 -0300

    Update NinjaModel.java

--- rest-with-spring-boot-and-java-produtoTest/src/main/java/br/com/erudio/Ninjas/NinjaModel.java
+++ rest-with-spring-boot-and-java-produtoTest/src/main/java/br/com/erudio/Ninjas/NinjaModel.java
@@ -4,6 +4,8 @@ package br.com.erudio.Ninjas;
 import br.com.erudio.Missoes.Missoes;
 import jakarta.persistence.*;
 
+import java.util.List;
+
 @Entity
 @Table(name = "tb_cadastro")
 public class NinjaModel {
