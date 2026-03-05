c9d847a9c1a2ffcc545861cfcb0dc158161a6137
Fernanda Dias <mariadosantodias@gmail.com>
2026-03-04 23:07:45 -0300

    Create RevisaoJpaApplicationTests.java

---
+++ revisao-JPA/src/test/java/com/TestJPA/revisao/JPA/RevisaoJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TestJPA.revisao.JPA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RevisaoJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
