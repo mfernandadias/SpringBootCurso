0e90de67c7ff060a5594072159d9fece6d22f781
Fernanda Dias <mariadosantodias@gmail.com>
2026-03-04 23:07:25 -0300

    Create RevisaoJpaApplication.java

---
+++ revisao-JPA/src/main/java/com/TestJPA/revisao/JPA/RevisaoJpaApplication.java
@@ -0,0 +1,13 @@
+package com.TestJPA.revisao.JPA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RevisaoJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RevisaoJpaApplication.class, args);
+	}
+
+}
