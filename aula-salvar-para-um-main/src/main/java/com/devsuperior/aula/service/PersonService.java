0429a4947935798bb5eed9c1b90b71c07a3d961a
Fernanda Dias <mariadosantodias@gmail.com>
2026-03-13 14:42:40 -0300

    Create PersonService.java

---
+++ aula-salvar-para-um-main/src/main/java/com/devsuperior/aula/service/PersonService.java
@@ -0,0 +1,23 @@
+package com.devsuperior.aula.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class PersonService {
+
+
+
+    //Entity → DTO
+    /*
+    Entity
+
+DTO
+Repository
+JPA (JPQL queries)
+
+Person
+Department
+Assim você verá DTO + JPA + Repository funcionando juntos.
+     */
+
+}
