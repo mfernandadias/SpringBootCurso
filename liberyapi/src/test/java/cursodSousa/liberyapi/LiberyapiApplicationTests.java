dc76bcc5acf14d52873402464b17493c74ffb10b
Fernanda Dias <mariadosantodias@gmail.com>
2026-03-13 14:47:42 -0300

    Create LiberyapiApplicationTests.java

---
+++ liberyapi/src/test/java/cursodSousa/liberyapi/LiberyapiApplicationTests.java
@@ -0,0 +1,13 @@
+package cursodSousa.liberyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiberyapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
