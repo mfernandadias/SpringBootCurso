c3ba8eebc3f872c3ef9b674ce7a9da89eb52ef2b
Fernanda Dias <mariadosantodias@gmail.com>
2026-03-13 14:47:07 -0300

    Create LiberyapiApplication.java

---
+++ liberyapi/src/main/java/cursodSousa/liberyapi/LiberyapiApplication.java
@@ -0,0 +1,13 @@
+package cursodSousa.liberyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiberyapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiberyapiApplication.class, args);
+	}
+
+}
