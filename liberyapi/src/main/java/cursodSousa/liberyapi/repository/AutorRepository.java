d0423554308450c9d0396ea42bf608b3735fbce2
Fernanda Dias <mariadosantodias@gmail.com>
2026-03-17 09:01:39 -0300

    Update AutorRepository.java

--- liberyapi/src/main/java/cursodSousa/liberyapi/repository/AutorRepository.java
+++ liberyapi/src/main/java/cursodSousa/liberyapi/repository/AutorRepository.java
@@ -1,12 +1,12 @@
 package cursodSousa.liberyapi.repository;
 
-import entites.Autor;
+import entites.Author;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
 @Repository
-public interface AutorRepository extends JpaRepository<Autor, UUID> {
+public interface AutorRepository extends JpaRepository<Author, UUID> {
 
 }
