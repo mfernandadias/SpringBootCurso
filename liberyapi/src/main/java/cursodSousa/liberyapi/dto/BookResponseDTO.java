5c38c84fe4bb483e1e0cf32785667ee4d4d1fd0b
Fernanda Dias <mariadosantodias@gmail.com>
2026-03-17 09:01:14 -0300

    Create BookResponseDTO.java

---
+++ liberyapi/src/main/java/cursodSousa/liberyapi/dto/BookResponseDTO.java
@@ -0,0 +1,12 @@
+package cursodSousa.liberyapi.dto;
+
+public class BookResponseDTO {
+
+    public Long id;
+    public String title;
+    public String isbn;
+    public Integer publicationYear;
+    public String authorName;
+    public Integer totalCopies;
+    public Integer availableCopies;
+}
