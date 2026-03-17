2ff171865d11c44ac62c312fc76bbdf4c7cbc506
Fernanda Dias <mariadosantodias@gmail.com>
2026-03-17 09:17:49 -0300

    atualização

--- dscommerce/src/main/java/com/devsuperior/dscommerce/repositories/CategoryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.devsuperior.dscommerce.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.devsuperior.dscommerce.entities.Category;
-
-public interface CategoryRepository extends JpaRepository<Category, Long> {
-
-}
--- dscommerce/src/main/java/com/devsuperior/dscommerce/repositories/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.devsuperior.dscommerce.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.devsuperior.dscommerce.entities.Order;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-
-}
