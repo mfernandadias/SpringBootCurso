c5f4b629c174a54c11ae0ba648a709ff6bae721d
Fernanda Dias <mariadosantodias@gmail.com>
2026-03-17 09:01:31 -0300

    Create LoanResponseDTO.java

---
+++ liberyapi/src/main/java/cursodSousa/liberyapi/dto/LoanResponseDTO.java
@@ -0,0 +1,11 @@
+package cursodSousa.liberyapi.dto;
+
+import java.time.LocalDate;
+
+public class LoanResponseDTO {
+    public Long id;
+    public String borrowername;
+    public String bookTitle;
+    public LocalDate  returnDate;
+
+}
