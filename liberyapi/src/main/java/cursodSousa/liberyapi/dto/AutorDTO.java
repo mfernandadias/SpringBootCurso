4bc921c7b8e165250d1701137e4b5544e61dd750
Fernanda Dias <mariadosantodias@gmail.com>
2026-03-17 09:01:05 -0300

    Update AutorDTO.java

--- liberyapi/src/main/java/cursodSousa/liberyapi/dto/AutorDTO.java
+++ liberyapi/src/main/java/cursodSousa/liberyapi/dto/AutorDTO.java
@@ -1,4 +1,21 @@
 package cursodSousa.liberyapi.dto;
 
+import entites.Author;
+
+import java.time.LocalDate;
+
 public class AutorDTO {
+    //AuthorRequestDTO
+    public Long id;
+    public String name;
+    public String nationality;
+    public LocalDate birthDate;
+
+    public AutorDTO(Author author) {
+        this.id = author.getId();
+        this.birthDate = author.getBirthDate();
+        this.nationality = author.getNationality();
+        this.name = author.getName();
+    }
+
 }
