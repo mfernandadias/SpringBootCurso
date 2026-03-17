e880d139cecc5292cca39d3834cba912d4633acc
Fernanda Dias <mariadosantodias@gmail.com>
2026-03-17 09:01:44 -0300

    Update LivroRepository.java

--- liberyapi/src/main/java/cursodSousa/liberyapi/repository/LivroRepository.java
+++ liberyapi/src/main/java/cursodSousa/liberyapi/repository/LivroRepository.java
@@ -1,6 +1,7 @@
 package cursodSousa.liberyapi.repository;
 
-import entites.Livro;
+import entites.Book;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -11,30 +12,36 @@ import java.util.List;
 import java.util.UUID;
 
 @Repository
-public interface LivroRepository extends JpaRepository<Livro, UUID> {
+public interface LivroRepository extends JpaRepository<Book, Long> {
     //JpaRepository já traz várias funções prontas
+    //oq isso realiza ?
+    Page<Book> findAll(org.springframework.data.domain.Pageable pageable);
+
+
+}
+
+/*
 
     //buscar livros caros
     //explicação melhor
     @Query("SELECT 1 FROM Livro 1 WHERE 1.preco > :valor")
-    List<Livro> livrosCaros(@Param("valor") BigDecimal valor);
+    List<Book> livrosCaros(@Param("valor") BigDecimal valor);
 
     //buscar por titulo
-    List<Livro> findByTituloContainingIgnoreCase(String titulo);
+    List<Book> findByTituloContainingIgnoreCase(String titulo);
 
     //buscar por isbn
-    List<Livro> findByIsbn(String isbn);
+    List<Book> findByIsbn(String isbn);
 
     //buscar por genero
-    List<Livro> findByGenero(String genero);
+    List<Book> findByGenero(String genero);
 
     //buscar por nome do autor
     @Query("""
-            SELECT 1 
-            FROM Livro 1 
-            JOIN 1.autor a 
+            SELECT 1
+            FROM Livro 1
+            JOIN 1.autor a
             WHERE LOWER(a.nome) LIKE LOWER(CONCAT('%', :nome, '%')
             """)
-    List<Livro> buscarPorAutor(@Param("nome") String nome);
-
-}
+    List<Book> buscarPorAutor(@Param("nome") String nome);
+ */
