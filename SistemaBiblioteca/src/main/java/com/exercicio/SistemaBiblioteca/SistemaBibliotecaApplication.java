0a8b452ca37f0e710faade1b6fcd0b6d374c6e69
Fernanda Dias <mariadosantodias@gmail.com>
2026-03-17 09:02:49 -0300

    Create SistemaBibliotecaApplication.java

---
+++ SistemaBiblioteca/src/main/java/com/exercicio/SistemaBiblioteca/SistemaBibliotecaApplication.java
@@ -0,0 +1,13 @@
+package com.exercicio.SistemaBiblioteca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaBibliotecaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaBibliotecaApplication.class, args);
+	}
+
+}
