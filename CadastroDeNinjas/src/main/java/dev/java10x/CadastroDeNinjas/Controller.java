823742a8b6347cbb7fff5f696f36e70c6b63b20b
Fernanda Dias <mariadosantodias@gmail.com>
2026-03-20 22:21:39 -0300

    Create Controller.java

---
+++ CadastroDeNinjas/src/main/java/dev/java10x/CadastroDeNinjas/Controller.java
@@ -0,0 +1,18 @@
+package dev.java10x.CadastroDeNinjas;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping
+@RestController
+
+public class Controller {
+
+    /*@GetMapping
+    public String boasVindas("/boasVindas/mensagem"){
+        return "Essa é minha primeira mensagem nessa rota";
+    }*/
+
+}
